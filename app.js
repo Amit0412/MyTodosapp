4854ac6d347db56a499daab1ea99bebc87f06ea7
Amit0412 <amitpandharikar@gmail.com>
2021-09-11 01:13:00 +0530

    updated

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+
+mongoose.connect("mongodb://localhost/mytodo", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+
+
+app.use(require("./routes/index"))
+app.use(require("./routes/todo"))
+
+
+
+app.listen(3002, () => console.log("Server started listening on port: 3000"));
